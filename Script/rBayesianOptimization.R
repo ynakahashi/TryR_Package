43f35db4078ffd639a8e81d3f0c4529865c616e7
ynakahashi <ynakahashi1003@gmail.com>
2017-05-17 22:22:38 +0900

    no message

--- Script/rBayesianOptimization.R
+++ Script/rBayesianOptimization.R
@@ -18,39 +18,139 @@ install.packages("rBayesianOptimization")
 library(rBayesianOptimization)
 
 ################################################################################
-## run sample script
+## simple simulation
 ################################################################################
 set.seed(123)
-simulate_y <- function(pars) {
+simulate_y_01 <- function(pars) {
    X1 <- rnorm(100, 100, 2) * rep(c(0, rep(1, 7), 0, 1), 10)
    X2 <- filter(X1, pars[1], "recursive")
    Z1 <- rnorm(100, 5, 2) * rep(c(rep(0, 5), rep(1, 5)), 10)
    dat <- data.frame(
-      "Y" = 50 + pars[2] * X2 + pars[3] * Z1 + rnorm(100, 0, pars[4]),
+      "Y"  = 50 + pars[2] * X2 + pars[3] * Z1 + rnorm(100, 0, pars[4]),
       "X1" = X1,
       "Z1" = Z1)
    return(dat)
 }
 
-return_AIC <- function(a) {
-   # dat     <- simulate_y(pars)
+return_AIC_01 <- function(a) {
    dat$Tmp <- filter(dat$X1, a, "recursive")
    AIC(lm(Y ~ Tmp + Z1, dat))
 }
 
-pars <- c(0.2, 0.5, 4, 0.3)
-dat  <- simulate_y(pars)
-optimise(return_AIC, c(0, 1))$minimum
+## low lambda
+pars      <- c(0.1, 0.5, 4, 5.0)
+n         <- 500
+res_01_01 <- matrix(0, n, 4)
+for (i in 1:n) {
+   dat <- simulate_y_01(pars)
+   a   <- optimise(return_AIC_01, c(0, 1))$minimum
+   X2  <- filter(dat$X1, a, "recursive")
+   res_01_01[i, ] <- c(a, coef(lm(Y ~ X2 + Z1, data = dat)))
+}
+summary(res_01_01)
+MASS::truehist(res_01_01[, 1])
 
 
-n     <- 500
-res_a <- matrix(0, n, 2)
+## High lambda
+pars      <- c(0.9, 0.5, 4, 5.0)
+n         <- 500
+res_01_02 <- matrix(0, n, 4)
+for (i in 1:n) {
+   dat <- simulate_y_01(pars)
+   a   <- optimise(return_AIC_01, c(0, 1))$minimum
+   X2  <- filter(dat$X1, a, "recursive")
+   res_01_02[i, ] <- c(a, coef(lm(Y ~ X2 + Z1, data = dat)))
+}
+summary(res_01_02)
+MASS::truehist(res_01_02[, 1])
+
+
+################################################################################
+## 2 variables
+################################################################################
+simulate_y_02 <- function(pars) {
+   X1 <- rnorm(100, 100, 2) * rep(c(0, rep(1, 7), 0, 1), 10)
+   X2 <- filter(X1, pars[1], "recursive")
+   Z1 <- rnorm(100, 5, 2) * rep(c(rep(0, 5), rep(1, 5)), 10)
+   Z2 <- filter(Z1, pars[2], "recursive")
+   dat <- data.frame(
+      "Y"  = 50 + pars[3] * X2 + pars[4] * Z2 + rnorm(100, 0, pars[5]),
+      "X1" = X1,
+      "Z1" = Z1)
+   return(dat)
+}
+
+return_AIC_02 <- function(param) {
+   a <- param[1]
+   b <- param[2]
+   dat$TmpX <- filter(dat$X1, a, "recursive")
+   dat$TmpZ <- filter(dat$Z1, b, "recursive")
+   AIC(lm(Y ~ TmpX + TmpZ, dat))
+}
+
+init_par <- array(c(0.5, 0.5))
+optim(par = optim(par = init_par, fn = return_AIC)$par, 
+      fn = return_AIC)$par
+
+## Low lambda
+pars      <- c(0.1, 0.1, 0.5, 4, 5.0)
+n         <- 100
+res_02_01 <- matrix(0, n, 5)
 for (i in 1:n) {
    dat <- simulate_y(pars)
-   a   <- optimise(return_AIC, c(0, 1))$minimum
-   X2  <- filter(dat$X1, a, "recursive")
-   res_a[i, ] <- c(a, coef(lm(Y ~ X2 + Z1, data = dat))[2])
+   res <- optim(par = optim(par = init_par, fn = return_AIC)$par, 
+                fn = return_AIC)$par
+   X2  <- filter(dat$X1, res[1], "recursive")
+   Z2  <- filter(dat$Z1, res[2], "recursive")
+   res_02_01[i, ] <- c(res, coef(lm(Y ~ X2 + Z2, data = dat)))
 }
-summary(res_a)
-MASS::truehist(res_a[, 1])
-MASS::truehist(res_a[, 2])
+summary(res_02_01)
+
+
+## High lambda
+pars      <- c(0.8, 0.7, 0.5, 4, 5.0)
+n         <- 100
+res_02_02 <- matrix(0, n, 5)
+for (i in 1:n) {
+   dat <- simulate_y(pars)
+   res <- optim(par = optim(par = init_par, fn = return_AIC)$par, 
+                fn = return_AIC)$par
+   X2  <- filter(dat$X1, res[1], "recursive")
+   Z2  <- filter(dat$Z1, res[2], "recursive")
+   res_02_02[i, ] <- c(res, coef(lm(Y ~ X2 + Z2, data = dat)))
+}
+summary(res_02_02)
+
+
+################################################################################
+## Bayesian Optimization
+################################################################################
+
+return_AIC_BO <- function(a, b) {
+   dat$TmpX <- filter(dat$X1, a, "recursive")
+   dat$TmpZ <- filter(dat$Z1, b, "recursive")
+   out <- list(Score = -AIC(lm(Y ~ TmpX + TmpZ, dat)),
+               Pred  = -AIC(lm(Y ~ TmpX + TmpZ, dat)))
+   return(out)
+}
+
+pars   <- c(0.1, 0.1, 0.5, 4, 5.0)
+dat    <- simulate_y(pars)
+res_BO <- BayesianOptimization(return_AIC_BO, bounds = list(a = c(0, 1), b = c(0, 1)),
+                               init_points = 20, n_iter = 1, acq = "ucb",
+                               kappa = 2.576, eps = 0, verbose = TRUE)
+
+init_par <- array(c(0.5, 0.5))
+optim(par = optim(par = init_par, fn = return_AIC)$par, 
+      fn = return_AIC)$par
+
+
+pars   <- c(0.8, 0.8, 0.5, 4, 5.0)
+dat    <- simulate_y(pars)
+res_BO <- BayesianOptimization(return_AIC_BO, bounds = list(a = c(0, 1), b = c(0, 1)),
+                               init_points = 100, n_iter = 2, acq = "ucb",
+                               kappa = 2.576, eps = 0, verbose = TRUE)
+
+init_par <- array(c(0.5, 0.5))
+optim(par = optim(par = init_par, fn = return_AIC)$par, 
+      fn = return_AIC)$par

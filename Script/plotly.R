cad6797d450cbe186e379e861f0dc69701ffaf29
ynakahashi <ynakahashi1003@gmail.com>
2017-05-24 23:21:18 +0900

    no message

--- Script/plotly.R
+++ Script/plotly.R
@@ -13,30 +13,47 @@
 work_dir <- "/Users/nakahashi/Desktop/GitTest/TryR_Package/Script"
 setwd(work_dir)
 
-## install & load prophet
+## install & load plotly
 # install.packages("plotly")
 library(plotly)
 
 ################################################################################
 ## Run sample script
 ################################################################################
-# histogram
+## histogram
 plot_ly(economics, x = ~ pop)
+
+## scatter plot with line
 plot_ly(economics, x = ~ date, y = ~ pop)
+
+## heat map
 plot_ly(z = ~ volcano)
+
+## 3D plot
 plot_ly(z = ~ volcano, type = "surface")
 
+## line chart in 2 ways
 add_lines(plot_ly(economics, x = ~ date, y = ~ unemploy/pop))
+economics %>% 
+   plot_ly(x = ~ date, y = ~ unemploy/pop) %>% 
+   add_lines()
 
-economics %>% plot_ly(x = ~ date, y = ~ unemploy/pop) %>% add_lines()
-
+## multiple line
 plot_ly(economics, x = ~ date, color = I("black")) %>%
    add_lines(y = ~ uempmed) %>%
    add_lines(y = ~ psavert, color = I("red"))
 
+## scatter plot for iris and ...
 p <- plot_ly(iris, x = ~ Sepal.Width, y = ~ Sepal.Length) 
+
+## different marker size, color, symbol and line
 add_markers(p, color = ~ Petal.Length, size = ~ Petal.Length)
 add_markers(p, color = ~ Species)
 add_markers(p, color = ~ Species, colors = "Set1")
 add_markers(p, symbol = ~ Species)
 add_paths(p, linetype = ~ Species)
+
+## export
+install.packages("htmlwidgets")
+htmlwidgets::saveWidget(p, "index.html")
+

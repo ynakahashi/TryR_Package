0b06fcca673da7ea9ccbbd88eea2210435d065a2
ynakahashi <ynakahashi1003@gmail.com>
2017-07-08 00:39:59 +0900

    Anscombe

--- Script/Anscombe.R
+++ Script/Anscombe.R
@@ -13,12 +13,8 @@
 work_dir <- "/Users/nakahashi/Desktop/GitTest/TryR_Package/Script"
 setwd(work_dir)
 
-## library install & load
-# install.packages("tweenr")
-# library(tweenr)
-# devtools::install_github("dgrtwo/gganimate")
-# library(gganimate)
 library(tidyverse)
+library(broom)
 library(forcats)
 library(ggplot2)
 
@@ -26,7 +22,8 @@ library(ggplot2)
 ################################################################################
 ### Run sample script
 ################################################################################
-datasaurus <- read_table2("../Data/SameStatsDataAndImages/datasets/DatasaurusDozen.tsv",
+## Datasaurus
+datasaurus <- read_table2("../Data/DatasaurusDozen.tsv",
                           col_names = TRUE, col_types = "cnn") %>%
    mutate(dataset = as_factor(dataset))
 
@@ -37,11 +34,36 @@ ggplot(datasaurus, aes(x = x, y = y)) +
    geom_point()
 
 
-# p <- ggplot(datasaurus, aes(x = x, y = y)) +
-#    geom_point(aes(frame = dataset))
-# 
-# animation::ani.options(interval = 1)
-# gganimate(p, title_frame = FALSE)
+datasaurus %>% 
+   group_by(dataset) %>% 
+   summarise_each(funs(mean, sd), vars = c(x, y))
 
+
+
+## Anscombe
+dataAns <- select(anscombe, "x" = x1, "y" = y1) %>% 
+   bind_rows(select(anscombe, "x" = x2, "y" = y2)) %>% 
+   bind_rows(select(anscombe, "x" = x3, "y" = y3)) %>% 
+   bind_rows(select(anscombe, "x" = x4, "y" = y4)) %>% 
+   mutate("dataset" = c(rep("I", nrow(anscombe)), rep("II", nrow(anscombe)),
+                        rep("III", nrow(anscombe)), rep("IV", nrow(anscombe))))
+
+dataAns %>% 
+   group_by(dataset) %>% 
+   summarise_each(funs(mean, sd), vars = c(x, y))
+
+dataAns %>% 
+   group_by(dataset) %>% 
+   summarise("COR" = cor(x, y))
+
+ggplot(dataAns, aes(x = x, y = y, label = dataset)) +
+   facet_wrap(~ dataset, nrow = 2) +
+   geom_point() +
+   geom_smooth(method = "lm", formula = y ~ x, se = FALSE)
+
+dataAns %>% 
+   split(.$dataset) %>% 
+   map(., ~ lm(y ~ x, data = .) %>% tidy()) %>% 
+   bind_rows()
 
 
